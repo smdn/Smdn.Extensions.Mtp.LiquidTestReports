b98a8ff80a1e91a87bc7517a7deabaf26804e187
smdn <smdn@smdn.jp>
2025-12-07 15:09:06 +0900

    make shim for KeyValuePair.Create internal

--- src/Smdn.Extensions.Mtp.LiquidTestReports/System.Collections.Generic/KeyValuePair.cs
+++ src/Smdn.Extensions.Mtp.LiquidTestReports/System.Collections.Generic/KeyValuePair.cs
@@ -3,7 +3,7 @@
 #if !SYSTEM_COLLECTIONS_GENERIC_KEYVALUEPAIR_CREATE
 namespace System.Collections.Generic;
 
-public static class KeyValuePair {
+internal static class KeyValuePair {
   public static KeyValuePair<TKey, TValue> Create<TKey, TValue>(TKey key, TValue value)
     => new(key, value);
 }
